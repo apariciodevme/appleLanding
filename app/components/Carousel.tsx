525f7fd1d1dfb84b76abf853247749980fab7369
apariciodevme <apariciodev@proton.me>
2024-12-23 00:53:50 +0100

    carousel blue print

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -1,45 +1,47 @@
-import React from 'react'
+import React from "react";
 
 const Carousel = () => {
   return (
-    <div className="carousel rounded-box">
-  <div className="carousel-item">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-      alt="Burger" />
-  </div>
-  <div className="carousel-item">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
-      alt="Burger" />
-  </div>
-  <div className="carousel-item">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
-      alt="Burger" />
-  </div>
-  <div className="carousel-item">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
-      alt="Burger" />
-  </div>
-  <div className="carousel-item">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
-      alt="Burger" />
-  </div>
-  <div className="carousel-item">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
-      alt="Burger" />
-  </div>
-  <div className="carousel-item">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
-      alt="Burger" />
-  </div>
-</div>
-  )
-}
+    <div className="overflow-hidden carousel carousel-center mx-w-md rounded-box">
+      <div className="flex flex-col items-start min-h-full shadow-xl carousel-item card bg-base-100 image-full">
+        <figure>
+          <img className="z-10" src="/carousel/posters/1.jpeg" alt="Shoes" />
+        </figure>
 
-export default Carousel
+        <div className="z-10 justify-between h-full gap-5 p-5 jus card-body">
+          <div className="flex flex-col items-center gap-5">
+            <img className="w-20" src="/carousel/tv.png" alt="" />
+            <img className="max-w-96" src="carousel/titles/1.png" alt="" />
+          </div>
+          <div className="flex flex-col items-center flex-none pb-10">
+            <div className="flex flex-col items-center mb-5">
+              <h2 className="text-2xl font-semibold text-white">Sci-Fi</h2>
+              <p className="text-sm text-white/80">New season.</p>
+            </div>
+            <button className="h-10 font-normal text-black rounded-full btn-sm btn">
+              Stream now
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+/*.carousel-item {
+  box-sizing: content-box;
+  display: flex;
+  flex: none;
+  scroll-snap-align: start;
+}
+.carousel-start .carousel-item {
+  scroll-snap-align: start;
+}
+.carousel-center .carousel-item {
+  scroll-snap-align: center;
+}
+.carousel-end .carousel-item {
+  scroll-snap-align: end;
+}*/
+
+export default Carousel;
--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   
     <CardDisplay/>
      
-    
+    <Carousel/>
     </main>
   );
 }
